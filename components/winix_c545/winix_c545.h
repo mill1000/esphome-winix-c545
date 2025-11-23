103e916ba08e1c084afd09d36be5e17a01bc6688
Tucker Kern <tuckkern@gmail.com>
2025-11-23 14:48:08 -0700

    Style fixes

--- components/winix_c545/winix_c545.h
+++ components/winix_c545/winix_c545.h
@@ -140,7 +140,7 @@ class WinixC545Fan : public fan::Fan, public Parented<WinixC545Component> {
   static constexpr const char *const PRESET_SLEEP = "Sleep";
   static constexpr const char *const PRESET_AUTO = "Auto";
 
-  bool presets_equal_(const char* preset_a, const char* preset_b) {
+  bool presets_equal_(const char *preset_a, const char *preset_b) {
     if (preset_a == nullptr || preset_b == nullptr)
       return false;
 
