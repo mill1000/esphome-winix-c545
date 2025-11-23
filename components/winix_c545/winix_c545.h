5cc533a7521457f22aea2c81266ce47fcf8e5d8a
Tucker Kern <tuckkern@gmail.com>
2025-11-23 11:38:32 -0700

    Update preset implementation for ESPHome 2025.11

--- components/winix_c545/winix_c545.h
+++ components/winix_c545/winix_c545.h
@@ -136,9 +136,16 @@ class WinixC545Fan : public fan::Fan, public Parented<WinixC545Component> {
   void update_state(const WinixStateMap &);
 
  protected:
-  const std::string PRESET_NONE{""};
-  const std::string PRESET_SLEEP{"Sleep"};
-  const std::string PRESET_AUTO{"Auto"};
+  static constexpr const char *const PRESET_NONE = "";
+  static constexpr const char *const PRESET_SLEEP = "Sleep";
+  static constexpr const char *const PRESET_AUTO = "Auto";
+
+  bool presets_equal_(const char* preset_a, const char* preset_b) {
+    if (preset_a == nullptr || preset_b == nullptr)
+      return false;
+
+    return strcmp(preset_a, preset_b) == 0;
+  }
 
   void control(const fan::FanCall &call) override;
   fan::FanTraits traits_;
